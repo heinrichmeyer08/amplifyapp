be843352bf22fce6c2f34a2c51411b26e6cd2f2d
heinrichmeyer08 <heinrichmeyer08@gmail.com>
2022-09-15 21:54:38 +0200

    changes for v2

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+/*
+Original video line
 import React from 'react';
 import './App.css';
 import logo from "./logo.svg";
@@ -24,3 +26,19 @@ function App({ signOut }) {
 }
 
 export default withAuthenticator(App);
+*/
+
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Hello from V2</h1>
+      </header>
+    </div>
+  );
+}
+export default App;
